77affc0b1679cb71197c44d6dd73d6759065d1b2
JR Ladson <jrladson@gmail.com>
2020-11-25 02:57:24 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number)
-  if number % 3 == 0
+  if (number % 3 == 0) && (number % 5 == 0)
+    "FizzBuzz"
+  elseif number % 3 == 0
     return "Fizz"
   elseif number % 5 == 0
     return "Buzz"
-  elseif (number % 3 == 0) && (number % 5 == 0)
-    "FizzBuzz"
-  end
+ end
 end
